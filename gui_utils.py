7dc4448e1e5627959117c77caaf0060e46c3ebbd
ilazoja <ilazoja@uwaterloo.ca>
2021-08-12 16:30:22 -0400

    Added play/pause button

---
+++ gui_utils.py
@@ -0,0 +1,10 @@
+import pygame
+import sys
+
+from pygame.locals import *
+
+def draw_text(text, font, color, surface, x, y):
+    textobj = font.render(text, 1, color)
+    textrect = textobj.get_rect()
+    textrect.topleft = (x, y)
+    surface.blit(textobj, textrect)
